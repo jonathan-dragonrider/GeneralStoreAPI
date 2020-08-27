00627771073d002dde926f790ce104019fd2dce5
Jon Kline <jonphilk@gmail.com>
2020-08-27 10:18:10 -0400

    initial commit, created models

--- GeneralStoreAPI/Models/Transaction.cs
+++ GeneralStoreAPI/Models/Transaction.cs
@@ -7,5 +7,13 @@ namespace GeneralStoreAPI.Models
 {
     public class Transaction
     {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+        public int ItemCount { get; set; }
+        public DateTime DateOfTransaction { get; set; }
+
     }
 }
